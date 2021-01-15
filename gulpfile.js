6b4c88c38dd52ab5e94fe1557abb944188888a41
six0860613 <58581054+six0860613@users.noreply.github.com>
2021-01-15 12:47:01 +0800

    add navbar-toggler auto close js

--- gulpfile.js
+++ gulpfile.js
@@ -35,9 +35,9 @@ function modules() {
     .pipe(gulp.dest('./vendor/bootstrap'));
   // jQuery
   var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
+    './node_modules/jquery/dist/*',
+    '!./node_modules/jquery/dist/core.js'
+  ])
     .pipe(gulp.dest('./vendor/jquery'));
   return merge(bootstrap, jquery);
 }
@@ -59,3 +59,4 @@ exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
+
